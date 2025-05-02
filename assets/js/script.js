ad019c6bf91ce0eb2e480bbf4ef11a4d718e9f59
RH Sadik <sadikcsbd@gmail.com>
2025-05-02 21:54:31 +0600

    details page

--- assets/js/script.js
+++ assets/js/script.js
@@ -96,9 +96,9 @@ $(".testimonial-carousel").owlCarousel({
     },
   },
 });
-// ===== Testimonial Slider End
+// ----- Testimonial Slider End
 
-// ===== Sticky Navbar
+// ----- Sticky Navbar
 window.addEventListener("scroll", function () {
   const element = document.querySelector(".mian_nav");
   if (window.scrollY > 300) {
@@ -121,3 +121,37 @@ mobileMenuClose.addEventListener("click", function () {
 });
 
 // ----- Mobile Menu Drawer End
+
+// ----- Details Images
+let slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides((slideIndex += n));
+}
+
+function currentSlide(n) {
+  showSlides((slideIndex = n));
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("my_slides");
+  let dots = document.getElementsByClassName("demo");
+  let captionText = document.getElementById("caption");
+  if (n > slides.length) {
+    slideIndex = 1;
+  }
+  if (n < 1) {
+    slideIndex = slides.length;
+  }
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex - 1].style.display = "block";
+  dots[slideIndex - 1].className += " active";
+  captionText.innerHTML = dots[slideIndex - 1].alt;
+}
